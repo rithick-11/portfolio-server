7e35646d5c1e1b68bf03d42158de8e56510409fd
rithick-11 <rithickroshan7878@gmail.com>
2024-08-04 05:18:36 +0530

    test server

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("server app")
+import express from "express"
+
+const app = express()
+
+app.get("/",(req, res) =>{
+    res.send("server working")
+})
+
+app.listen(3009, () => {
+    console.log("server running at 3033");
+})
