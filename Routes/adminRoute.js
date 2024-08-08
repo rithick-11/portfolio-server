4368aa4246e3ac93a12ec842edd669667ecf362c
rithick-11 <rithickroshan7878@gmail.com>
2024-08-08 16:34:27 +0530

    dashboard api fix

--- Routes/adminRoute.js
+++ Routes/adminRoute.js
@@ -1,8 +1,12 @@
 import { Router } from "express";
 import Project from "../Model/ProjectModal.js";
 import Admin from "../Model/adminModel.js";
+import vistorCount from "../Model/visterCount.js";
+import Contact from "../Model/contactModel.js";
 
 const router = Router();
+
+
 
 router.post("/add/user", async (req, res) => {
   const newAdmin = await Admin.create(req.body);
@@ -13,12 +17,12 @@ router.post("/login", async (req, res) => {
   const daUser = await Admin.findOne({ username: req.body.username });
   if (daUser !== null) {
     if (daUser.password === req.body.password) {
-      res.json(true);
+      res.json({token:"lklaslaksdaldlsdlasdlalklsmdmal;km",msg:"login success fully"});
     } else {
-      res.status(403).json({ msg: "worng Password" });
+      res.status(404).json({ msg: "worng Password" });
     }
   } else {
-    res.status(403).    json({ msg: "user not found" });
+    res.status(404).json({ msg: "user not found" });
   }
 });
 
@@ -27,5 +31,12 @@ router.post("/add/project", async (req, res) => {
   const newProject = await Project.create(userData);
   res.json(newProject);
 });
+
+router.get("/vist-count", async (req, res) => {
+  const count = await vistorCount.findOne({ _id: "66b1c9183f675b3f2078b814" }).sort({RecentVist:1});
+  const messages = await Contact.find().sort({createdAt: -1})
+  res.json({count:count.count, recentVist: count.RecentVist, messages});
+});
+
 
 export default router;
