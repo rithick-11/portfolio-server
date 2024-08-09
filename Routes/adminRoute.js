029e7dcc256a425859b9f3ff6cd42339f2422f56
rithick-11 <rithickroshan7878@gmail.com>
2024-08-10 00:56:24 +0530

    added dashboard api

--- Routes/adminRoute.js
+++ Routes/adminRoute.js
@@ -3,6 +3,7 @@ import Project from "../Model/ProjectModal.js";
 import Admin from "../Model/adminModel.js";
 import vistorCount from "../Model/visterCount.js";
 import Contact from "../Model/contactModel.js";
+import User from "../Model/UserModel.js"
 
 const router = Router();
 
@@ -37,6 +38,34 @@ router.get("/vist-count", async (req, res) => {
   const messages = await Contact.find().sort({createdAt: -1})
   res.json({count:count.count, recentVist: count.RecentVist, messages});
 });
+
+router.get("/user-detial", async (req, res) => {
+  try{
+    const userData = await User.find({},{password:0}).sort({createdAt:-1})
+  res.json({users:userData, total: userData.length} )
+  }catch(err){
+    res.json({msg:"we cannot feed data"})
+  }
+})
+
+router.get("/projects", async (req, res) => {
+  try{
+    const projectData = await Project.find({},{name:-1,likedUser:-1}).sort({order:1})
+    const data = projectData.map((each) => ({id:each._id, name:each.name, likes: each.likedUser.length}))
+    res.json({projects:data, total:projectData.length})
+  }catch(err){
+    res.json({msg:"we cannot feed data"})
+  }
+})
+
+router.get("/project/:id", async (req, res) => {
+  try{
+    const projectData = await Project.findOne({_id:req.params.id}).sort({order:1})
+    res.json({projects:projectData, total:projectData.length})
+  }catch(err){
+    res.json({msg:"we cannot feed data"})
+  }
+})
 
 
 export default router;
